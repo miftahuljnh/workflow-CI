f05cd4b2598854f8a2360069f5e0d5d51f1b440b
miftahuljnh <miftahuljannahmft1311@gmail.com>
2025-12-25 20:50:47 +0700

    Update modelling.py

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,21 +1,14 @@
 import mlflow
 import mlflow.sklearn
 import pandas as pd
-import os
-import sys
-import io
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, recall_score
+import os
 
-sys.stdout = io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
-
-data_path = "diabetes_preprocessing/diabetes.csv"
-
-if not os.path.exists(data_path):
-    data_path = "Kriteria 2/diabetes_preprocessing/diabetes.csv"
-
+data_path = "Kriteria 2/diabetes_preprocessing/diabetes.csv"
 df = pd.read_csv(data_path)
+
 X = df.drop(columns=["Outcome"])
 y = df["Outcome"]
 
@@ -23,27 +16,21 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42, stratify=y
 )
 
-mlflow.set_experiment("Diabetes_Production_Model")
+mlflow.sklearn.autolog()
 
-with mlflow.start_run(run_name="final_model_deployment"):
-  
-    mlflow.sklearn.autolog()
+clf = RandomForestClassifier(n_estimators=200, max_depth=12, random_state=42)
+clf.fit(X_train, y_train)
 
-    clf = RandomForestClassifier(n_estimators=200, max_depth=12, random_state=42)
-    clf.fit(X_train, y_train)
+y_pred = clf.predict(X_test)
+acc = accuracy_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
 
-    y_pred = clf.predict(X_test)
-    acc = accuracy_score(y_test, y_pred)
-    recall = recall_score(y_test, y_pred)
+# Simpan model ke folder lokal
+mlflow.sklearn.save_model(
+    sk_model=clf,
+    path="mlflow_model_local",
+    input_example=X_test.iloc[:1]
+)
 
-
-    mlflow.sklearn.log_model(
-        sk_model=clf,
-        artifact_path="model",
-        input_example=X_test.iloc[:1]
-    )
-
-    print(f"--- Evaluasi Model ---")
-    print(f"Accuracy : {acc:.4f}")
-    print(f"Recall   : {recall:.4f}")
-    print(f"----------------------")
+print(f"Accuracy : {acc:.4f}")
+print(f"Recall   : {recall:.4f}")
